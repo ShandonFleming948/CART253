7492fb0960e638144edc418b457388e5c243b273
ShandonFleming948 <shandonflem@gmail.com>
2020-09-22 22:12:12 -0400

    creating circle 2

--- CART 253 - activities/03-moving-objects/03-moving-pictures-template/js/script.js
+++ CART 253 - activities/03-moving-objects/03-moving-pictures-template/js/script.js
@@ -40,6 +40,17 @@ function draw() {
   bg.r = bg.r + 1;
 
 //left circle
+  circle1.x = circle1.x + 1
+  circle1.x = constrain(circle1.x,0,width/2);
+  circle1.size = circle1.size + 1;
+  circle1.size = constrain(circle1.size,0,width);
   fill(circle1.fill,circle1.alpha);
   ellipse(circle1.x,circle1.y,circle1.size);
+
+//right circle
+  circle2.x = circle2.x + - 1
+  circle2.x = constrain(circle2.x,width/2,width);
+  circle2.size = circle1.size * 0.75;
+  fill(circle2.fill,circle2.alpha);
+  ellipse(circle2.x,circle2.y,circle2.size);
 }
