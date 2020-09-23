42d9765fcb950c50dfefeacc8fce5aeeb5afb2fa
ShandonFleming948 <shandonflem@gmail.com>
2020-09-23 14:57:17 -0400

    E1. setting up background and circles

--- CART 253 - exercises/exercise 1/js/script.js
+++ CART 253 - exercises/exercise 1/js/script.js
@@ -2,9 +2,23 @@
 Exercise 01: I like to move it move it!
 Shandon Fleming
 
-Brief: Create a program full of shapes that move, change size and color, and anything else you want to experiment with.
+Brief: Create a program full of shapes that move, change size and color,
+and anything else you want to experiment with.
 **************************************************/
+let bg = {
+  r: 0,
+  b: 0,
+  g: 0
+}
+let circle1 = {
+  
+}
+let circle2 = {
 
+}
+let circle3 = {
+
+}
 // setup()
 //
 // Description of setup() goes here.
