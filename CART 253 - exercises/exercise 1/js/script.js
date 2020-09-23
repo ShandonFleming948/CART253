e33e80f5dc19ab3e365d666ac2266618b7692642
ShandonFleming948 <shandonflem@gmail.com>
2020-09-23 16:34:32 -0400

    E1. arranging the movement of the 3 circles

--- CART 253 - exercises/exercise 1/js/script.js
+++ CART 253 - exercises/exercise 1/js/script.js
@@ -15,7 +15,8 @@ let circle1 = {
   x:100,
   y:500,
   size:15,
-  fill:250
+  fill:250,
+  speed:0.1
 };
 let circle2 = {
   x:300,
@@ -27,7 +28,8 @@ let circle3 = {
   x:500,
   y:500,
   size:15,
-  fill:250
+  fill:250,
+  speed:0.1
 };
 // setup()
 //
@@ -44,20 +46,27 @@ function draw() {
   background(bg.r,bg.g,bg.b);
 
 //circle1
-  circle1.y = mouseY
-  circle1.y = constrain(circle1.y,100,500);
+  circle1.y = random(0,600);
+  circle1.x = random(0,600);
+  circle1.y = constrain(circle1.y,100,550);
+  circle1.y = constrain(circle1.x,100,550);
   fill(circle1.fill);
   ellipse(circle1.x,circle1.y,circle1.size);
 
 //circle2
   circle2.y = mouseY
-  circle2.y = constrain(circle2.y,100,500);
+  circle2.x = mouseX
+  circle2.y = constrain(circle2.y,10,590);
+  circle2.y = constrain(circle2.y,100,550);
   fill(circle2.fill);
   ellipse(circle2.x,circle2.y,circle2.size);
 
 //circle3
-  circle3.y = mouseY
-  circle3.y = constrain(circle3.y,100,500);
+  circle3.y = circle3.y + circle3.speed;
+  circle3.y = random(0,600);
+  circle3.x = random(0,600);
+  circle3.y = constrain(circle3.y,100,550);
+  circle3.x = constrain(circle3.x,100,550);
   fill(circle3.fill);
   ellipse(circle3.x,circle3.y,circle3.size);
 }
