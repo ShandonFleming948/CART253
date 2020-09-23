9e8244921e01a960fc8bc899e5e505a56b464165
ShandonFleming948 <shandonflem@gmail.com>
2020-09-23 15:17:36 -0400

    E1. customized background color

--- CART 253 - exercises/exercise 1/js/script.js
+++ CART 253 - exercises/exercise 1/js/script.js
@@ -6,9 +6,9 @@ Brief: Create a program full of shapes that move, change size and color,
 and anything else you want to experiment with.
 **************************************************/
 let bg = {
-  r: 0,
-  b: 0,
-  g: 0
+  r: 70,
+  b: 217,
+  g: 224
 }
 let circle1 = {
   x:100,
@@ -40,5 +40,5 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(bg.r,bg.g,bg.b);
 }
