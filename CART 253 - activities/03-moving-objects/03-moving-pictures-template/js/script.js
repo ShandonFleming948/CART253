07f555706e62b9d1f2db8d68d948ee383a42e7fc
ShandonFleming948 <shandonflem@gmail.com>
2020-09-22 22:25:27 -0400

    Completed activity 3

--- CART 253 - activities/03-moving-objects/03-moving-pictures-template/js/script.js
+++ CART 253 - activities/03-moving-objects/03-moving-pictures-template/js/script.js
@@ -13,6 +13,8 @@ let circle1 = {
 x: 0,
 y: 250,
 size: 100,
+speed: 1,
+growthRate: 1,
 fill: 255,
 alpha: 200
 };
@@ -20,6 +22,8 @@ let circle2 = {
 x: 500,
 y: 250,
 size: 75,
+speed: -1,
+growthRatio: 0.5,
 fill: 255,
 alpha: 200
 };
@@ -37,20 +41,20 @@ function setup() {
 function draw() {
 //background
   background(bg.r,bg.g,bg.g);
-  bg.r = bg.r + 1;
+  bg.r = map(circle1.size,100,width,0,255);
 
 //left circle
-  circle1.x = circle1.x + 1
+  circle1.x = circle1.x + circle1.speed;
   circle1.x = constrain(circle1.x,0,width/2);
-  circle1.size = circle1.size + 1;
+  circle1.size = circle1.size + circle1.growthRate;
   circle1.size = constrain(circle1.size,0,width);
   fill(circle1.fill,circle1.alpha);
   ellipse(circle1.x,circle1.y,circle1.size);
 
 //right circle
-  circle2.x = circle2.x + - 1
+  circle2.x = circle2.x + circle2.speed;
   circle2.x = constrain(circle2.x,width/2,width);
-  circle2.size = circle1.size * 0.75;
+  circle2.size = circle1.size * circle2.growthRatio;
   fill(circle2.fill,circle2.alpha);
   ellipse(circle2.x,circle2.y,circle2.size);
 }
