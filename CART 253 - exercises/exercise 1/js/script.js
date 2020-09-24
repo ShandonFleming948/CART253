ecf5ba34c6b5393f3165478de558af6c417e3617
ShandonFleming948 <shandonflem@gmail.com>
2020-09-24 16:38:35 -0400

    E1. customized the animations of the circles

--- CART 253 - exercises/exercise 1/js/script.js
+++ CART 253 - exercises/exercise 1/js/script.js
@@ -46,27 +46,29 @@ function draw() {
 //circle1
   circle1.y = mouseY
   circle1.x = mouseX
-  circle1.size = circle1.y;
+  circle1.size = circle1.y - 100;
   circle1.size = constrain(circle1.size,0,100);
-  circle1.y = constrain(circle1.y,10,590);
+  circle1.y = constrain(circle1.y,10,550);
   circle1.x = constrain(circle1.x,90,110);
   fill(circle1.fill);
   ellipse(circle1.x,circle1.y,circle1.size);
 
 //circle2
-  fill(circle2.fill);
   circle2.y = mouseY
   circle2.x = mouseX
   circle2.size = circle2.y - 200;
-  circle2.size = constrain(circle2.size,0,200);
-  circle2.y = constrain(circle2.y,10,590);
+  circle2.size = constrain(circle2.size,0,125);
+  circle2.y = constrain(circle2.y,10,550);
   circle2.x = constrain(circle2.x,290,310);
+  fill(circle2.fill);
   ellipse(circle2.x,circle2.y,circle2.size);
 
 //circle3
   circle3.y = mouseY
   circle3.x = mouseX
-  circle3.y = constrain(circle3.y,10,590);
+  circle3.size = circle3.y - 300;
+  circle3.size = constrain(circle3.size,0,150);
+  circle3.y = constrain(circle3.y,10,550);
   circle3.x = constrain(circle3.x,490,510);
   fill(circle3.fill);
   ellipse(circle3.x,circle3.y,circle3.size);
