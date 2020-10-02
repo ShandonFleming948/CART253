f42beb40025a51d1f7fdf5e3e02fe460d4624756
ShandonFleming948 <shandonflem@gmail.com>
2020-10-01 23:15:25 -0400

    E2. Customized covid19

--- CART 253 - exercises/exercise 2/js/script.js
+++ CART 253 - exercises/exercise 2/js/script.js
@@ -25,7 +25,11 @@ function preLoad() {
 }
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(windowWidth,windowHeight);
+
+  covid19.y = random(0,height);
+  covid19.vx = covid19.speed;
+
 }
 
 // draw()
@@ -34,5 +38,9 @@ function setup() {
 function draw() {
   background(0);
 
-  image()
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  ellipse(covid19.x,covid19.y,covid19.size);
 }
