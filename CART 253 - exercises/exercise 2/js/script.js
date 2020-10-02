413156d71cdf97a8ea9dac37697885a74bda6227
ShandonFleming948 <shandonflem@gmail.com>
2020-10-01 23:03:48 -0400

    E2. created covid19

--- CART 253 - exercises/exercise 2/js/script.js
+++ CART 253 - exercises/exercise 2/js/script.js
@@ -4,6 +4,20 @@ Exercise 02 - Dodge-em
 
 **************************************************/
 
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 200,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 250,
+    g: 0,
+    b: 0
+  }
+}
+
 let clownFace;
 
 function preLoad() {
@@ -11,12 +25,14 @@ function preLoad() {
 }
 
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  image()
 }
