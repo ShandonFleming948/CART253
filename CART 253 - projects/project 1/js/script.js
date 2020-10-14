1133c757bef25d48ef0233fe12a4beefe20ebcbe
ShandonFleming948 <shandonflem@gmail.com>
2020-10-14 17:04:44 -0400

    P1. Started coding the maze

--- CART 253 - projects/project 1/js/script.js
+++ CART 253 - projects/project 1/js/script.js
@@ -19,4 +19,48 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(52,177,235);
+
+  line(0,500,100,500);
+  stroke(0);
+  strokeWeight(10);
+
+  line(200,400,200,600);
+  stroke(0);
+  strokeWeight(10);
+
+  line(100,400,200,400);
+  stroke(0);
+  strokeWeight(10);
+
+  line(0,300,300,300);
+  stroke(0);
+  strokeWeight(10);
+
+  line(300,300,300,400);
+  stroke(0);
+  strokeWeight(10);
+
+  line(400,400,400,600);
+  stroke(0);
+  strokeWeight(10);
+
+  line(100,200,400,200);
+  stroke(0);
+  strokeWeight(10);
+
+  line(200,0,200,100);
+  stroke(0);
+  strokeWeight(10);
+
+  line(300,100,300,200);
+  stroke(0);
+  strokeWeight(10);
+
+  line(400,100,400,200);
+  stroke(0);
+  strokeWeight(10);
+
+  line(400,100,500,100);
+  stroke(0);
+  strokeWeight(10);
 }
