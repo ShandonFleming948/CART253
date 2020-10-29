304abc5cdb6608fe5162917b31afce3b75c2c120
ShandonFleming948 <shandonflem@gmail.com>
2020-10-29 12:55:09 -0400

    E5. Customized the bouncing of the balls

--- CART 253 - exercises/exercise 5/js/Ball.js
+++ CART 253 - exercises/exercise 5/js/Ball.js
@@ -25,14 +25,24 @@ move() {
 
   this.x = this.x + this.vx;
   this.y = this.y + this.vy;
-}
 
-bounce() {
-  if (this.y + this.size/2 >= height) {
-    this.vy = -this.vy;
-    this.ay = 0;
+  if (this.y - this.size/2 > height) {
+    this.active = false;
   }
 }
+
+bounce(paddle) {
+  if (this.x > paddle.x - paddle.width/2 &&
+      this.x < paddle.x + paddle.width/2 &&
+      this.y + this.size/2 > paddle.y - paddle.height/2 &&
+      this.y - this.size/2 < paddle.y + paddle.height/2) {
+      //Bounce
+      let dx = this.x - paddle.x;
+      this.vx = this.vx + map(dx,-paddle.width/2,paddle.width/2,-2,2);
+      this.vy = -this.vy;
+      this.ay = 0;
+    }
+  }
 
 display() {
   push();
