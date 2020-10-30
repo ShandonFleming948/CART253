fb870a394071b70dad83df24f923a086ae1a9da9
ShandonFleming948 <shandonflem@gmail.com>
2020-10-29 22:46:56 -0400

    E5. Setting up the states and the simulation

--- CART 253 - exercises/exercise 5/js/script.js
+++ CART 253 - exercises/exercise 5/js/script.js
@@ -20,6 +20,8 @@ let numBalls = 3;
 let bigBalls = [];
 let numBigBalls = 2;
 
+let state = `title`;
+
 function setup() {
   createCanvas(1260,610);
 
@@ -44,7 +46,7 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+  background(26, 186, 240);
 
   paddle.move();
   paddle.display();
@@ -70,9 +72,74 @@ function draw() {
     }
   }
 
+  //4 states
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `simulation`) {
+    simulation();
+  }
+  else if (state === `win`) {
+    win();
+  }
+  else if (state === `lose`) {
+    lose();
+  }
 }
+
+  function title() {
+    push();
+    textSize(24);
+    fill(0,0,250);
+    textAlign(CENTER,CENTER);
+    text(`Juggle the balls until only the red balls/a red ball is left`,width/2,height/2);
+    pop();
+  }
+
+  function simulation() {
+    handleInput();
+    move();
+    display();
+    checkActive();
+  }
+
+//display "lose" page if user touches a red circle
+  function lose() {
+    push();
+    textSize(64);
+    fill(0,0,250);
+    textAlign(CENTER,CENTER);
+    text(`YOU LOST ALL THE RED BALLS :(`,width/2,height/2);
+    pop();
+  }
+
+//display "win" page if user makes it to the green square
+  function win() {
+    push();
+    textSize(50);
+    fill(0,0,250);
+    textAlign(CENTER,CENTER);
+    text(`WELL DONE, YOU DID IT :)`,width/2,height/2);
+    pop();
+  }
+
 
   function move() {
     paddle.x = paddle.x + paddle.vx;
     paddle.x = constrain(paddle.x,0,width);
+  }
+
+  function mousePressed() {
+    if (state ===`title`) {
+    state = `simulation`;
+    }
+  }
+
+  function checkActive() {
+    if (ball.active === true && bigBall.active === false) {
+      state = `win`;
+    }
+    else {
+      state = `lose`;
+    }
   }
