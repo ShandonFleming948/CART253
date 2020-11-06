3efb9042210b2432d5ae8242afee989640c37328
ShandonFleming948 <shandonflem@gmail.com>
2020-11-06 01:35:38 -0400

    PTP: Put together a "prototype"

--- CART 253 - projects/project 2 prototype/js/script.js
+++ CART 253 - projects/project 2 prototype/js/script.js
@@ -1,22 +1,36 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Shandon Fleming
+CART 253 - Project 2 "Prototype"
 **************************************************/
+//circle color
+let circleColor = fill(51,51,51);
 
-// setup()
-//
-// Description of setup() goes here.
+//created the canvas
 function setup() {
-  createCanvas(1250,600);
+  createCanvas(1250, 600);
+
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
-  background(230, 232, 225);
+  background(220);
+
+//supposed to be an example of a wall that the user's circle would come into contact with
+  push();
+  line(0, 250, 1250, 250);
+  strokeWeight(5);
+  pop();
+
+//the user's circle
+  fill(51);
+  circle(mouseX,mouseY,30);
+
+//supposed to be the moving wall :|
+  line(700,300,700,500);
+}
+
+//supposed to be a "working" if statement that changes the color of the user's circle when it touches the wall
+if (circle.y = 250) {
+  circleColor === fill(250,0,0);
 }
