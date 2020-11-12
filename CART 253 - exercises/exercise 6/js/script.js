2cdee83180d999f819ba501f8964c91c3f4b0877
ShandonFleming948 <shandonflem@gmail.com>
2020-11-12 17:46:42 -0400

    E6: Corrected oscillator glitch

--- CART 253 - exercises/exercise 6/js/script.js
+++ CART 253 - exercises/exercise 6/js/script.js
@@ -9,31 +9,33 @@ Here is a description of this template p5 project.
 let oscillator;
 
 let circleUser = {
-  x:300,
-  y:300,
-  size:50,
-  vx:1,
-  vy:1,
-  speed:4
+  x: 300,
+  y: 300,
+  size: 50,
+  vx: 1,
+  vy: 1,
+  speed: 4
 }
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(600,600);
+  createCanvas(600, 600);
 
-  oscillator = new p5.Oscillator(440,`sine`);
-  oscillator.amp(0.0001);
+  oscillator = new p5.Oscillator(440, `sine`);
+  oscillator.start();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(52,177,235);
+  background(52, 177, 235);
 
   handleInput();
   move();
+  sing();
+
   display();
 }
 
@@ -64,48 +66,35 @@ function move() {
   circleUser.x = circleUser.x + circleUser.vx;
   circleUser.y = circleUser.y + circleUser.vy;
 
-  circleUser.x = constrain(circleUser.x,50,550);
-  circleUser.y = constrain(circleUser.y,50,550);
+  circleUser.x = constrain(circleUser.x, 50, 550);
+  circleUser.y = constrain(circleUser.y, 50, 550);
 
 }
 
 function display() {
   noStroke()
   fill(250);
-  ellipse(circleUser.x,circleUser.y,circleUser.size);
+  ellipse(circleUser.x, circleUser.y, circleUser.size);
 }
 
-function keyPressed() {
-  // if (keyIsDown(LEFT_ARROW) || (keyIsDown(RIGHT_ARROW)) || (keyIsDown(UP_ARROW)) || (keyIsDown(DOWN_ARROW))) {
-  //   oscillator.start();
-  // }
-
-  if (keyIsDown(RIGHT_ARROW)) {
-    oscillator = new p5.Oscillator(400,`sine`);
-    oscillator.start();
+function sing() {
+  if (circleUser.vx < 0) {
+    oscillator.freq(300);
+    oscillator.amp(0.08);
   }
-
-  else if (keyIsDown(UP_ARROW)) {
-    oscillator = new p5.Oscillator(500,`sine`);
-    oscillator.start();
+  else if (circleUser.vx > 0) {
+    oscillator.freq(400);
+    oscillator.amp(0.08);
   }
-
-  else if (keyIsDown(LEFT_ARROW)) {
-    oscillator = new p5.Oscillator(300,`sine`);
-    oscillator.start();
+  else if (circleUser.vy < 0) {
+    oscillator.freq(500);
+    oscillator.amp(0.08);
   }
-
-  else if (keyIsDown(DOWN_ARROW)) {
-    oscillator = new p5.Oscillator(200,`sine`);
-    oscillator.start();
+  else if (circleUser.vy > 0) {
+    oscillator.freq(200);
+    oscillator.amp(0.08);
   }
-
-}
-
-function keyReleased() {
-  console.log(oscillator);
-  if (oscillator) {
-    oscillator.stop();
-    oscillator = undefined;
+  else {
+    oscillator.amp(0);
   }
 }
